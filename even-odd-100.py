b994d82f1b8522842841fddbfdc2992db843d1c2
nik-viii <69429371+nik-viii@users.noreply.github.com>
2021-06-01 02:24:58 +0530

    Delete even-odd-100.py

--- even-odd-100.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter the integer: "))
-if(n < 100):
-    if(n % 2 == 0):
-        print(str(n)+" is even.")
-    else:
-        print(str(n)+" is odd.")
-else:
-    print("Number is not less than 100.")
